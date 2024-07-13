0df5d69e5b3ccaeef121078e01413e86a2a51931
us3r64bit <arth.prajapati@outlook.com>
2024-07-13 17:31:07 +0530

    Improve Navbar Add Convex

--- app/(marketing)/_components/Navbar.tsx
+++ app/(marketing)/_components/Navbar.tsx
@@ -4,16 +4,40 @@ import { useScrollTop } from "@/hooks/use-scroll-top";
 import { cn } from "@/lib/utils";
 import { Logo } from "./logo";
 import { ModeToggle } from "@/components/mode-toggle";
+import { useConvexAuth } from "convex/react";
+import { SignInButton } from "@clerk/clerk-react";
+import { Button } from "@/components/ui/button";
+import { Spinner } from "@/components/spinner";
 
 const Navbar = () => {
+  const{isAuthenticated, isLoading} = useConvexAuth();
   const scrolled = useScrollTop();
   return(
     <div className={cn(
         "fixed top-0 z-50 flex w-full items-center bg-white p-6 dark:bg-[#1f1f1f]",
         scrolled && "border-b shadow-sm",
-    )}>
+      )}>
         <Logo/>
         <div className="flex w-full items-center justify-between gap-x-2 md:ml-auto md:justify-end">
+            {
+              isLoading && <Spinner/>
+            }
+            {
+              !isAuthenticated && !isLoading && (
+                <>
+                  <SignInButton>
+                    <Button variant={"ghost"}>
+                      Log in
+                    </Button>
+                  </SignInButton>
+                  <SignInButton>
+                    <Button>
+                      Get Notebook free
+                    </Button>
+                  </SignInButton>
+                </>
+              )
+            }
             <ModeToggle/>
         </div>
     </div>
---
+++ convex/auth.config.js
@@ -0,0 +1,8 @@
+export default {
+  providers: [
+    {
+      domain: "https://grown-caiman-13.clerk.accounts.dev",
+      applicationID: "convex",
+    },
+  ],
+};
