b6f2d4fb39bfcda113ffeb072b0a6df5b305496f
us3r64bit <arth.prajapati@outlook.com>
2024-07-22 22:33:15 +0530

    Nested File Manager(sidebar)

--- app/(main)/_components/Item.tsx
+++ app/(main)/_components/Item.tsx
@@ -1,9 +1,19 @@
 "use client";
 
 import { Skeleton } from "@/components/ui/skeleton";
+import { api } from "@/convex/_generated/api";
 import { Id } from "@/convex/_generated/dataModel";
 import { cn } from "@/lib/utils";
-import { ChevronDown, ChevronRight, LucideIcon } from "lucide-react";
+import { useMutation } from "convex/react";
+import {
+  ChevronDown,
+  ChevronRight,
+  LucideIcon,
+  Plus,
+  Trash2Icon,
+} from "lucide-react";
+import { useRouter } from "next/navigation";
+import { toast } from "sonner";
 
 interface ItemProps {
   id?: Id<"documents">;
@@ -30,14 +40,59 @@ const Item = ({
   onExpand,
   expanded,
 }: ItemProps) => {
+  const create = useMutation(api.documents.create);
+  const router = useRouter();
   const ChevronIcon = expanded ? ChevronDown : ChevronRight;
+  const archived = useMutation(api.documents.archived);
+
+  const handleExpand = (
+    event: React.MouseEvent<HTMLDivElement, MouseEvent>,
+  ) => {
+    event.preventDefault();
+    event.stopPropagation();
+    onExpand?.();
+  };
+  const handleArchived = (
+    event: React.MouseEvent<HTMLDivElement, MouseEvent>,
+  ) => {
+    event.preventDefault();
+    event.stopPropagation();
+    if (!id) return;
+    const promise = archived({ id: id }).then((documentId) => {
+      toast.promise(promise, {
+        loading: "Creating a new note...",
+        success: "New note created!",
+        error: "Failed to create a note.",
+      });
+      // router.push(`/documents/${documentId}`);
+    });
+  };
+
+  const handleCreate = (
+    event: React.MouseEvent<HTMLDivElement, MouseEvent>,
+  ) => {
+    event.preventDefault();
+    event.stopPropagation();
+    if (!id) return;
+    const promise = create({ title: "New Note", parentDocument: id }).then(
+      (documentId) => {
+        if (!expanded) {
+          onExpand?.();
+        }
+        toast.promise(promise, {
+          loading: "Moving to Archived...",
+          success: "Note Moved to Archived!",
+          error: "Failed to Move in Archived.",
+        });
+        // router.push(`/documents/${documentId}`);
+      },
+    );
+  };
   return (
     <div
       onClick={() => {}}
       role="button"
-      style={{
-        paddingLeft: "12px",
-      }}
+      style={{ paddingLeft: level ? `${level * 12 + 12}px` : "12px" }}
       className={cn(
         "group flex min-h-[27px] w-full items-center py-1 pr-3 text-sm font-medium text-muted-foreground hover:bg-primary/5",
         active && "bg-primary/5 text-primary",
@@ -45,6 +100,7 @@ const Item = ({
     >
       {!!id && (
         <div
+          onClick={(e) => handleExpand(e)}
           role="button"
           className="mr-1 h-full rounded-sm hover:bg-neutral-300 dark:bg-neutral-600"
         >
@@ -62,6 +118,24 @@ const Item = ({
         <kbd className="pointer-events-none ml-auto inline-flex h-5 select-none items-center gap-1 rounded border bg-muted px-1.5 font-mono text-[10px] font-medium text-muted-foreground opacity-100">
           ctrl + K
         </kbd>
+      )}
+      {!!id && (
+        <div className="ml-auto flex items-center gap-x-2">
+          <div
+            role="button"
+            onClick={(e) => handleCreate(e)}
+            className="opacity-0 group-hover:opacity-100 h-full ml-auto rounded-sm hover:bg-neutral-200 dark:hover:bg-neutral-600"
+          >
+            <Plus className="h-4 w-4 text-muted-foreground" />
+          </div>
+          <div
+            role="button"
+            onClick={(e) => handleArchived(e)}
+            className="opacity-0 group-hover:opacity-100 h-full ml-auto rounded-sm hover:bg-neutral-200 dark:hover:bg-neutral-600"
+          >
+            <Trash2Icon className="h-4 w-4 text-muted-foreground hover:text-red-500" />
+          </div>
+        </div>
       )}
     </div>
   );
--- convex/documents.ts
+++ convex/documents.ts
@@ -1,5 +1,45 @@
+import { Id } from "./_generated/dataModel";
 import { mutation, query } from "./_generated/server";
 import { v } from "convex/values";
+
+export const archived = mutation({
+  args: {
+    id: v.id("documents"),
+  },
+  handler: async (ctx, args) => {
+    const indentity = await ctx.auth.getUserIdentity();
+    if (!indentity) {
+      throw new Error("User not authenticated");
+    }
+    const userId = indentity.subject;
+    const existingDocument = await ctx.db.get(args.id);
+    if (!existingDocument) {
+      throw new Error("Not Found");
+    }
+    if (existingDocument.userId != userId) {
+      throw new Error("Unauthorized!!");
+    }
+    const recursiveArchive = async (documentId: Id<"documents">) => {
+      const children = await ctx.db
+        .query("documents")
+        .withIndex("by_user_parent", (q) =>
+          q.eq("userId", userId).eq("parentDocument", documentId),
+        )
+        .collect();
+
+      for (const child of children) {
+        await ctx.db.patch(child._id, {
+          isArchived: true,
+        });
+      }
+    };
+    const document = await ctx.db.patch(args.id, {
+      isArchived: true,
+    });
+    recursiveArchive(args.id);
+    return document;
+  },
+});
 
 export const getSidebar = query({
   args: {
